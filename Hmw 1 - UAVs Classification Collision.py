eb48c49002e9727e4264f71eedbb029e60bcf08f
SimoneBodi <36970900+SimoneBodi@users.noreply.github.com>
2022-11-19 18:48:58 +0100

    Update
    Now the code func with perceptron, bernoulliNB, GaussianNB, LogisticRegression

--- Hmw 1 - UAVs Classification Collision.py
+++ Hmw 1 - UAVs Classification Collision.py
@@ -3,24 +3,23 @@
 Created on Sun Nov 13 10:13:02 2022
 
 @author: Simone
+
 """
 
 # Import libraries that contains the implementations of the functions used in the rest of the program.
 
-import random
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import *
+from sklearn.naive_bayes import *
+from sklearn.metrics import confusion_matrix, classification_report
 import numpy as np
-from sklearn import svm
-from sklearn import preprocessing
 import matplotlib.pyplot as plt
-from sklearn.linear_model import Perceptron
-import sklearn.metrics 
-from sklearn import datasets
-from sklearn.model_selection import train_test_split, cross_val_score, ShuffleSplit, GridSearchCV
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.utils.multiclass import unique_labels
-import matplotlib.pyplot as plt
-
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.linear_model import Perceptron
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split, cross_val_score, ShuffleSplit, GridSearchCV
 
 def import_dataset():
     # This function open the dataset to store in a dictonary
@@ -50,13 +49,13 @@ def import_dataset():
                 elif num_f < len(column_dataset_X):
                     # Assign feature value to row_r and feature_f
                     List_of_feature.append(float(feature))
-            normalized_array = np.array(List_of_feature)
-            # normalized_array = preprocessing.normalize([np.array(List_of_feature)])
+            #normalized_array = np.array(List_of_feature)
+            normalized_array = preprocessing.normalize([np.array(List_of_feature)])
             X.append(normalized_array) #----------> NORMALIZING ARRAY?
         file.close()  # To change file access modes
     return X, np.array(T)
 
-    
+
 def split_dataset(X,T):
     # This function split the dataset in train set e test set
     # Split the data
@@ -65,26 +64,63 @@ def split_dataset(X,T):
     return X_train, X_test, y_train, y_test
 
 
-class LeastSquare:
-    
-    def __init__(self):
-        self.w = [0, 0, 0]
-
-    def fit(self,X,t):
-        n = len(X) # nr. of examples
-        t2 = np.c_[t, 1-t] # t2 is T: 1-of-K encoding
-        phi = np.c_[np.ones(n), X] # design matrix
-        self.w = np.matmul(np.linalg.pinv(phi),t2) # Least square solution
-        print("Least square solution: %s" %(str(self.w.transpose())))
-
-    def predict(self,x):
-        xn = np.array((1, x[0][0], x[0][1]))
-        yn = np.matmul(self.w.transpose(),xn)
-        if yn[0]>yn[1]:
-            return 1
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
         else:
-            return -1   
-        
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #print("Normalized confusion matrix")
+    else:
+        pass
+        #print('Confusion matrix, without normalization')
+
+    #print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    ax.set_ylim(len(classes)-0.5, -0.5)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+
 
 class SimplePerceptron:
 
@@ -117,147 +153,63 @@ class SimplePerceptron:
         xn = np.array((1, x[0][0], x[0][1]))
         yn = np.matmul(self.w.transpose(),xn)        
         return np.sign(yn)
-    
-    
-class FisherDiscriminant:
-
-    def __init__(self):
-        self.w = [0, 0, 0]
-        self.label = "Fisher Discriminant"
-
-    def fit(self,X,t):
-        n = len(X)  # num of examples
-        # group the two subsets 
-        # C1 = positive samples, C2 = negative samples
-        C1 = np.ndarray((0,2))
-        C2 = np.ndarray((0,2))
-        for i in range(0,len(X)):
-            if (t[i][0] == 1):
-                C1 = np.vstack([C1, [X[i,0],X[i,1]]])
-            else:
-                C2 = np.vstack([C2, [X[i,0],X[i,1]]])			
-        
-        # compute means m1, m2
-        m1 = np.mean(C1, axis=0)
-        m2 = np.mean(C2, axis=0)
-        
-        # compute covariances S1, S2
-        S1 = np.zeros((2,2))
-        d = np.array(())
-        for c in C1:
-            d = np.subtract(c,m1).reshape(2,1)
-            dt = d.transpose()
-            S1 = S1 + np.matmul(d,dt)
-        
-        S1 = S1/len(C1);
-        
-        S2 = np.zeros((2,2))
-        for c in C2:
-            d = np.subtract(c,m2).reshape(2,1)
-            dt = d.transpose()
-            S2 = S2 + np.matmul(d,dt)
-        S2 = S2/len(C2);
-        
-        # compute Sw matrix
-        Sw = S1+S2
-        
-        # compute solution w 
-        wt = np.matmul(np.linalg.inv(Sw),(m1-m2))
-        
-        # global mean
-        mu = m1 * 0.5 + m2 * 0.5
-        
-        # compute constant term
-        w0 = np.dot(wt,mu)
-        
-        # format the final solution
-        self.w = np.array([-w0, wt[0], wt[1]])
-        print("Fisher discriminant solution: %s" %str(self.w.transpose()))
 
     
-    def predict(self,x):
-        xn = np.array((1, x[0][0], x[0][1]))
-        yn = np.matmul(self.w.transpose(),xn)
-        if yn>0:
-            return 1
-        else:
-            return -1
-       
-   
-def CodeProf():     
-    # Param: n=size of data set, outliers=True/False
-    def generateData(n, outliers=False):
-        X = np.ndarray((n,2))
-        t = np.ndarray((n,1))
-        n1 = int(n*0.5)
     
-        # define random centers of disctributions far away
-        
-        for i in range(0,n1):
-            X[i,:] = np.random.normal((2.0,8.5),0.5,size=(1,2))
-            t[i] = -1
-        for i in range(n1,n):
-            X[i,:] = np.random.normal((4.0,5.0),0.3,size=(1,2))
-            t[i] = 1
-        
-        if (outliers):
-            no=int(n*0.9)
-            for i in range (no,n):
-                X[i,:] = np.random.normal((9.0,3.0),0.2,size=(1,2))
-                t[i] = 1
-    
-        return [X,t]
-    
-    n = 100
-    outliers = True
-    np.random.seed(123)
-    X, t = generateData(n, outliers=outliers)
-    return X, t
-    
-if __name__ == '__main__':
+if __name__ == '__main__':  # Main Programm
     print('Run the program ...\n')
+    
     
     # Format the dataset
     X,t = import_dataset()
     
-
+    # list of class
+    class_names = np.array([str(c) for c in set(t)])
     # X,t = CodeProf()
     
     # Adjust the dataset
     X_train, X_test, y_train, y_test = split_dataset(X,t)
     
-    # Chose the model
-    #classifier_name = 'S'
-    ClassifierMap = {
-        'L': [LeastSquare, 'Least Square'], 
-        'F': [FisherDiscriminant, 'Fisher Discriminant'], 
-        'p': [SimplePerceptron, 'Simple Perceptron'], 
-        'P': [Perceptron, 'Perceptron'], 
-        'S': [svm.LinearSVC, 'SVM']
-        }
+    # resahpe all dataset
+    dimX1, dimX2, dimX3 = np.array(X_train).shape
+    X_train = np.reshape(np.array(X_train), (dimX1*dimX2, dimX3))
     
-    choosed_model = input('Chose the model beetween: \n Least Square: L; \n Fisher Discriminant: F; \n Simple Perceptron: p; \n Perceptron: P; \n SVM: S. \n --> ')
+    dimX1, dimX2, dimX3 = np.array(X_test).shape
+    X_test = np.reshape(np.array(X_test), (dimX1*dimX2, dimX3))
     
-    # Train the model choosed
-    classifier = ClassifierMap[choosed_model][0]()
+    dimY2 = y_train.shape
+    y_train = np.reshape(np.array(y_train), dimY2)
     
-    #  Set further parameters if using Simple Perceptron
-    ETA = 0.001
-    NITER = 1000
-    if (choosed_model == 'p'):  # SimplePerceptron
-        classifier.eta = ETA
-        classifier.niter = NITER
-        
-    # Fit Classifier
-    # train the classifier
-    classifier.fit(X_train,y_train)
+    # model = Perceptron() # accuracy 0.53
+    # eta = 0.001
+    # niter = 1000
+    # model.eta = eta
+    # model.niter = niter    
+    # model = BernoulliNB() # accuracy 0.51
+    # model = GaussianNB() # accuracy 0.48
+    # model = LogisticRegression() #accuracy 0.53
+    model = svm.SVC(kernel='linear', C=4) # accuracy 0.55 best model
     
-    # Prediction for Least Square
-    y_pred = classifier.predict(x)
     
-    # evaluate Accuracy
-    acc = classifier.score(X_test, y_test)    
-    print("\nAccuracy %.3f" %acc)
+
+    #Train
+    model.fit(X_train, y_train)
     
-    # evluate Precision and Recall
-    #print(classification_report(y_test, y_pred, labels=None, target_names=t, digits=3))
+    #Test
+    y_pred = model.predict(X_test)
+    
+    # print evaluation matrics
+    print(classification_report(y_test, y_pred, labels=None, target_names= class_names, digits=2))
+    
+    # print confusion matrix    
+    cm = confusion_matrix(y_test, y_pred, labels=None, sample_weight=None)
+    print(cm)
+    plt.rcParams["figure.figsize"] = (10,10)
+    plot_confusion_matrix(y_test, y_pred, classes=class_names, normalize=False)
+    
+    #K-Fold Cross Validation 
+    # cv = ShuffleSplit(n_splits=5, test_size=0.333, random_state=15)
+    # scores = cross_val_score(model, X, t, cv=cv)
+    # print(scores)
+    # print("Accuracy: %0.3f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+  
